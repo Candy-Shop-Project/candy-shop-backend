6f480791a36cc13cda7357997e633678f6fdc595
Fluffy1908 <pro1shinov.tima@gmail.com>
2024-09-23 19:09:00 -0400

    removed delete_category view

--- candyShop/shop/urls.py
+++ candyShop/shop/urls.py
@@ -5,7 +5,7 @@ from .views import product_list, add_product, delete_product, update_product, ad
 urlpatterns = [
     path('products/', product_list, name='product_list'), #endpoint to query all product entries
     path('add_product/', add_product, name='add-product'), #add a new product to db
-    path('add_category/', add_category, name='add_category'),  # endpoint to add new categories (pass json in body {name: category})
+    path('add_category/', add_category, name='add_category'),  # endpoint to add new categories (pass json in body {"name": "category"})
     path('delete_product/<int:product_id>/', delete_product, name='delete_product'), # delete product by id, passed from frontend
     path('update_product/<int:product_id>/', update_product, name='update_product'), # update product by id, passed from frontend
 ]
