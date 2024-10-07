550c2e93d5c8ee5fb1be4e139fdbf33c78b77903
Fluffy1908 <pro1shinov.tima@gmail.com>
2024-10-07 17:35:07 -0400

    added price_id field to product model for stripe checkout process

--- candyShop/shop/models.py
+++ candyShop/shop/models.py
@@ -12,6 +12,7 @@ class Product(models.Model):
     name = models.CharField(max_length=100) # product name
     description = models.TextField() # product description
     price = models.DecimalField(max_digits=10, decimal_places=2)
+    price_id = models.CharField(max_length=100, blank=True, null=True)  # stripe price id
     image_url = models.URLField(max_length=200) # image URL storage
     category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products', default=1) # category model relation (id). Defaults to 1 for previous data entries, or if new product created but no value for category provided
     created_at = models.DateTimeField(auto_now_add=True)  # timestamp for product creation
