ca9af57d07dd7523ba5d5d3742210380739baa16
Fluffy1908 <pro1shinov.tima@gmail.com>
2024-10-08 18:48:07 -0400

    some changes to cors and debug

--- candyShop/candyShop/settings.py
+++ candyShop/candyShop/settings.py
@@ -36,7 +36,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 SECRET_KEY = env('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['seahorse-app-fjmlu.ondigitalocean.app', 'localhost', '127.0.0.1']
 
@@ -172,4 +172,6 @@ DJOSER = {
 }
 
 
-CORS_ALLOW_ALL_ORIGINS = True  # allows all origins for development purposes for now
+CORS_ALLOWED_ORIGINS = [
+    "https://seahorse-app-fjmlu.ondigitalocean.app/",
+]
