da0bef0a99592d53859098b6964becf334fad994
Fluffy1908 <pro1shinov.tima@gmail.com>
2024-12-03 22:19:10 -0500

    added env variable allowed_URL for easier use

--- candyShop/candyShop/settings.py
+++ candyShop/candyShop/settings.py
@@ -38,7 +38,7 @@ SECRET_KEY = env('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['urchin-app-pk38b.ondigitalocean.app', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = [env('ALLOWED_URL'), 'localhost', '127.0.0.1']
 
 
 # Application definition
